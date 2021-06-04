1c9b8414cdfc2445b906a2e9bf39f7d6be791ac9
fabrod3030 <62773995+fabrod3030@users.noreply.github.com>
2021-06-03 20:32:32 -0400

    Add: react router

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { NavLink } from "react-router-dom"
+
+function NavBar() {
+    return (
+        <header>
+            <div>
+                <nav>
+                    <NavLink>
+                        
+                    </NavLink>
+                </nav>
+            </div>
+        </header>
+    )
+}
+
+export default NavBar
---
+++ src/components/SinglePost.js
@@ -0,0 +1,11 @@
+import React from 'react'
+//rcfe
+function SinglePost() {
+    return (
+        <div>
+            SinglePost
+        </div>
+    )
+}
+
+export default SinglePost
